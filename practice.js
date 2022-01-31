d2e35d1e9fd819bb5888ed3c8fc15b7f02c6f0c0
ritika626 <ritika.sharma626@gmail.com>
2022-01-31 19:05:05 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,8 @@
+// const getUser=user=>{name:user.name,age:user.age};
+// const user={name:"ritika",age:24};
+// console.log(getUser(user));
+
+console.log('practice.js');
+// import {sum} from './sum';
+ const sum = require('./sum');
+console.log(sum(1,2));
---
+++ sandbox.js
@@ -0,0 +1,2 @@
+let name1 ='ritika';
+console.log(5+7-"7")
